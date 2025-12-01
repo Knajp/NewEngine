bac08917dc45e4a26aa63e644efa657978b3a1e0
Knajp <jcfhjcftug@gmail.com>
2025-12-01 20:05:39 +0100

    Inital Commit

---
+++ premake5.lua
@@ -0,0 +1,25 @@
+workspace "NewEngine"
+   configurations { "Debug", "Release" }
+   toolset "clang"
+
+project "NewEngine"
+   kind "ConsoleApp"
+   language "C++"
+   targetdir "bin/%{cfg.buildcfg}"
+
+   files { "**.h", "**.c", "**.cpp", "**.hpp" }
+
+   includedirs {"vendor/include"}
+   libdirs {"vendor/lib"}
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+`
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
+
+   filter "toolset:clang"
+      buildoptions { "-std=c++17", "-Wall" }
+      linkoptions { "-stdlib=libc++" }
